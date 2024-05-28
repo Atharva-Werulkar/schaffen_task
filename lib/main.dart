abd72db2a0da43273a307c701d4807f9b90a9e0b
Atharva Werulkar <werulkaratharva@gmail.com>
2024-05-29 01:22:40 +0530

    Initial Commit

---
+++ lib/main.dart
@@ -0,0 +1,335 @@
+import 'package:expandable_text/expandable_text.dart';
+import 'package:flutter/material.dart';
+import 'package:flutter/services.dart';
+import 'package:schaffen_task/utils/constants.dart';
+
+void main() {
+  runApp(const MyApp());
+
+  SystemChrome.setSystemUIOverlayStyle(const SystemUiOverlayStyle(
+    // Set status bar color
+    statusBarColor: Colors.white,
+    // Set status bar icon brightness
+    statusBarIconBrightness: Brightness.dark,
+    // Set system navigation bar color
+    systemNavigationBarColor: Colors.white,
+    // Set system navigation bar icon brightness
+    systemNavigationBarIconBrightness: Brightness.dark,
+  ));
+}
+
+class MyApp extends StatelessWidget {
+  const MyApp({super.key});
+
+  // This widget is the root of your application.
+  @override
+  Widget build(BuildContext context) {
+    return const MaterialApp(
+      title: 'Flutter Demo',
+      home: MyHomePage(),
+    );
+  }
+}
+
+class MyHomePage extends StatefulWidget {
+  const MyHomePage({super.key});
+
+  @override
+  State<MyHomePage> createState() => _MyHomePageState();
+}
+
+class _MyHomePageState extends State<MyHomePage> {
+  @override
+  Widget build(BuildContext context) {
+    bool _showAllChips = false;
+    bool _isSwitched = false;
+    return SafeArea(
+      child: Scaffold(
+        body: CustomScrollView(
+          slivers: <Widget>[
+            //app bar
+            SliverAppBar(
+              pinned: true,
+              snap: false,
+              floating: false,
+              expandedHeight: getDeviceHeight(context) * 0.25,
+              flexibleSpace: FlexibleSpaceBar(
+                background: Stack(
+                  fit: StackFit.expand,
+                  children: <Widget>[
+                    Image.asset(
+                      'assets/images/image.png',
+                      fit: BoxFit.cover,
+                    ),
+                    const DecoratedBox(
+                      decoration: BoxDecoration(
+                        gradient: LinearGradient(
+                          begin: Alignment(0.0, 0.5),
+                          end: Alignment(0.0, 0.0),
+                          colors: <Color>[
+                            Color(0x60000000),
+                            Color(0x00000000),
+                          ],
+                        ),
+                      ),
+                    ),
+                    //back button
+                    Align(
+                      alignment: Alignment.topLeft,
+                      child: Container(
+                        height: 40,
+                        width: 40,
+                        margin: const EdgeInsets.all(10),
+                        decoration: const BoxDecoration(
+                          shape: BoxShape.circle,
+                          color: Colors.black45,
+                        ),
+                        child: IconButton(
+                          icon:
+                              const Icon(Icons.arrow_back, color: Colors.white),
+                          onPressed: () {
+                            Navigator.pop(context);
+                          },
+                        ),
+                      ),
+                    ),
+                  ],
+                ),
+              ),
+            ),
+
+            //name with rating and share button
+            SliverToBoxAdapter(
+              child: Container(
+                height: getDeviceHeight(context) * 0.1,
+                color: Colors.red,
+                //name with rating and share button on the left corner
+                child: Row(
+                  mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                  children: <Widget>[
+                    const Padding(
+                      padding: EdgeInsets.symmetric(
+                          horizontal: 15.0, vertical: 15.0),
+                      child: Column(
+                        mainAxisAlignment: MainAxisAlignment.spaceEvenly,
+                        crossAxisAlignment: CrossAxisAlignment.start,
+                        children: <Widget>[
+                          Text(
+                            'The Weeknd',
+                            style: TextStyle(
+                              fontSize: 20.0,
+                              color: Colors.white,
+                              fontWeight: FontWeight.bold,
+                            ),
+                          ),
+                          Text('Community • +11K Members',
+                              style: TextStyle(
+                                fontSize: 14.0,
+                                color: Colors.white,
+                              ))
+                        ],
+                      ),
+                    ),
+                    Padding(
+                      padding: const EdgeInsets.symmetric(
+                          horizontal: 15.0, vertical: 15.0),
+                      child: Image.asset(
+                        height: 40.0,
+                        width: 40.0,
+                        'assets/images/share.png',
+                        fit: BoxFit.cover,
+                      ),
+                    ),
+                  ],
+                ),
+              ),
+            ),
+
+            //description
+            const SliverToBoxAdapter(
+              child: Padding(
+                padding: EdgeInsets.symmetric(horizontal: 15.0, vertical: 15.0),
+                child: Column(
+                  mainAxisAlignment: MainAxisAlignment.spaceEvenly,
+                  crossAxisAlignment: CrossAxisAlignment.start,
+                  children: <Widget>[
+                    ExpandableText(
+                      'Lorem ipsum dolor sit amet, consectetur adipiscing elit. '
+                      'Sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. '
+                      'Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. '
+                      'Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. '
+                      'Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.',
+                      expandText: 'Read more',
+                      collapseText: 'Show less',
+                      maxLines: 5,
+                      linkColor: Colors.red,
+                      style: TextStyle(
+                        fontSize: 14.0,
+                        color: Colors.black,
+                      ),
+                    ),
+                  ],
+                ),
+              ),
+            ),
+
+            //chips of categories
+            SliverToBoxAdapter(
+              child: SizedBox(
+                height: getDeviceHeight(context) * 0.08,
+                child: ListView.builder(
+                  physics: const BouncingScrollPhysics(),
+                  padding: const EdgeInsets.symmetric(
+                    horizontal: 10.0,
+                  ),
+                  scrollDirection: Axis.horizontal,
+                  itemCount: 5,
+                  itemBuilder: (BuildContext context, int index) {
+                    if (!_showAllChips && index == 4) {
+                      return GestureDetector(
+                          onTap: () {
+                            setState(() {
+                              _showAllChips = true;
+                            });
+                          },
+                          child: const Padding(
+                            padding: EdgeInsets.symmetric(
+                                horizontal: 5.0, vertical: 10.0),
+                            child: Chip(
+                              shape: RoundedRectangleBorder(
+                                borderRadius:
+                                    BorderRadius.all(Radius.circular(50.0)),
+                              ),
+                              side: BorderSide(
+                                color: Color(0xffEF456F),
+                              ),
+                              label: Text('+1'),
+                              labelStyle: TextStyle(
+                                color: Color(0xffEF456F),
+                              ),
+                            ),
+                          ));
+                    } else {
+                      return Padding(
+                        padding: const EdgeInsets.symmetric(
+                            horizontal: 5.0, vertical: 10.0),
+                        child: Chip(
+                          shape: const RoundedRectangleBorder(
+                            borderRadius:
+                                BorderRadius.all(Radius.circular(50.0)),
+                          ),
+                          side: const BorderSide(
+                            color: Color(0xffEF456F),
+                          ),
+                          label: Text('Outdoor $index'),
+                          labelStyle: const TextStyle(
+                            color: Color(0xffEF456F),
+                          ),
+                        ),
+                      );
+                    }
+                  },
+                ),
+              ),
+            ),
+
+            //list of Media, docs and links
+            SliverToBoxAdapter(
+              child: Column(
+                children: <Widget>[
+                  Row(
+                    children: [
+                      const Padding(
+                        padding: EdgeInsets.symmetric(
+                            horizontal: 15.0, vertical: 15.0),
+                        child: Text(
+                          'Media, docs and links',
+                          style: TextStyle(
+                            fontSize: 20.0,
+                            color: Colors.black,
+                            fontWeight: FontWeight.w500,
+                          ),
+                        ),
+                      ),
+                      const Spacer(),
+                      Padding(
+                        padding: const EdgeInsets.symmetric(
+                            horizontal: 15.0, vertical: 15.0),
+                        child: IconButton(
+                          onPressed: () {},
+                          icon: const Icon(
+                            Icons.navigate_next,
+                            size: 30.0,
+                          ),
+                        ),
+                      ),
+                    ],
+                  ),
+                  SizedBox(
+                    height: getDeviceHeight(context) * 0.2,
+                    child: ListView.builder(
+                      physics: const BouncingScrollPhysics(),
+                      padding: const EdgeInsets.symmetric(
+                        horizontal: 10.0,
+                      ),
+                      scrollDirection: Axis.horizontal,
+                      itemCount: 10,
+                      itemBuilder: (BuildContext context, int index) {
+                        return Padding(
+                          padding: const EdgeInsets.symmetric(
+                              horizontal: 5.0, vertical: 10.0),
+                          child: Container(
+                            height: getDeviceHeight(context) * 0.2,
+                            width: getDeviceWidth(context) * 0.35,
+                            decoration: BoxDecoration(
+                              image: DecorationImage(
+                                image: AssetImage('assets/images/image.png'),
+                                fit: BoxFit.cover,
+                              ),
+                              borderRadius: BorderRadius.circular(10.0),
+                            ),
+                          ),
+                        );
+                      },
+                    ),
+                  ),
+                ],
+              ),
+            ),
+
+            //Mute Notifications text with switch
+            SliverToBoxAdapter(
+              child: Padding(
+                padding: const EdgeInsets.symmetric(
+                    horizontal: 15.0, vertical: 15.0),
+                child: Row(
+                  mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                  children: <Widget>[
+                    const Text(
+                      'Mute notification',
+                      style: TextStyle(
+                        fontSize: 20.0,
+                        color: Colors.black,
+                        fontWeight: FontWeight.w500,
+                      ),
+                    ),
+                    Switch(
+                      value: _isSwitched,
+                      onChanged: (bool value) {
+                        setState(() {
+                          //change the value of switch
+                          _isSwitched = value;
+                        });
+                      },
+                      activeColor: Colors.red,
+                    ),
+                  ],
+                ),
+              ),
+            ),
+          ],
+        ),
+      ),
+    );
+  }
+}
---
+++ lib/utils/constants.dart
@@ -0,0 +1,10 @@
+//device height and width
+import 'package:flutter/material.dart';
+
+double getDeviceHeight(BuildContext context) {
+  return MediaQuery.of(context).size.height;
+}
+
+double getDeviceWidth(BuildContext context) {
+  return MediaQuery.of(context).size.width;
+}
